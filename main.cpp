ccaaeeb710440bc074e65e606671aa017eac6860
Dave <david00100johnson@gmail.com>
2022-07-13 20:03:16 -0700

    currently working on->library bugs

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
-#include <iostream>
+#include <stdio.h>
+#include <gsl_rng.h>
+#include <gsl_randist.h>
 
-int main(int argc, char const *argv[])
+int main (int argc, char *argv[])
 {
-    std::cout << char(99) << "++11" << std::endl;
-    return 0;
+  /* set up GSL RNG */
+  gsl_rng *r = gsl_rng_alloc(gsl_rng_mt19937);
+  /* end of GSL setup */
+
+  int i,n;
+  double gauss,gamma;  
+
+  n=atoi(argv[1]);
+  for (i=0;i<n;i++)
+    {
+      gauss=gsl_ran_gaussian(r,2.0);
+      gamma=gsl_ran_gamma(r,2.0,3.0);
+      printf("%2.4f %2.4f\n", gauss,gamma);
+    }
+  return(0);
 }
